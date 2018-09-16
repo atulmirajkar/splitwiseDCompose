51710b5fec8a33c7cb7954c198e9ae0667b29f53
atul <atulmirajkar@gmail.com>
2018-09-16 11:56:54 -0500

    stop and remove exiting containers and images

--- splitwisePrj.sh
+++ splitwisePrj.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 git clone https://github.com/atulmirajkar/splitwise.git 
 git clone https://github.com/atulmirajkar/splitwiseExpenseAPI.git
+mkdir data
+cp ./splitwiseExpenseAPI/config.json ./data/splitwiseconfig.json
+docker rm -f $(docker ps -a -q)
+docker rmi -f $(docker images -a -q)
